b7febf6dd9d58cdd325ee7a219e5855bf28a3674
aronunlabeled <xaron97x@hotmail.com>
2021-10-07 11:42:32 -0400

    me

--- branchWork/Program.cs
+++ branchWork/Program.cs
@@ -6,7 +6,7 @@ namespace branchWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("new branchsssssss");
+            Console.WriteLine("merger");
         }
     }
 }
